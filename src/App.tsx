06743dd8c2f863076456692bdadf5bf9491503cd
Olajide Balogun <olajidebalogun@icloud.com>
2023-12-03 09:54:48 +0100

    routing

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Main } from './pages/main';
+import { Login } from './pages/login';
+import { Navbar } from './components/navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
