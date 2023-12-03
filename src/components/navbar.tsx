95773d4dba030ef2483fa8a050d1c57e3d442133
Olajide Balogun <olajidebalogun@icloud.com>
2023-12-03 11:07:05 +0100

    firebase setup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
-
+import {auth} from "../config/firebase";
+import { useAuthState } from 'react-firebase-hooks/auth'
 export const Navbar = () => {
+   const [user] = useAuthState(auth);
   return (
   <div> 
+  <div className="Links"> 
     <Link to="/">Home</Link>
     <Link to="/login">Login</Link>
+  </div>
+
+    <div className="user">
+      <p>{auth.currentUser?.displayName}</p>
+      <img src={auth.currentUser?.photoURL || ""} width="100" height="100" />
+    </div>
   </div>
   )
 }
---
+++ src/config/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD1QVToyDBGDvhqrMLnxApeInX-x0rKkrg",
+  authDomain: "offi-20a57.firebaseapp.com",
+  projectId: "offi-20a57",
+  storageBucket: "offi-20a57.appspot.com",
+  messagingSenderId: "483164136335",
+  appId: "1:483164136335:web:e8f19823303b73bb8be1fa",
+  measurementId: "G-CBTK5BPT69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
