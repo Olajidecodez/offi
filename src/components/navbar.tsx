dd6409a5fdc64000823a5d2997535fa9f9180476
Olajide Balogun <olajidebalogun@icloud.com>
2023-12-05 20:32:04 +0100

    css for navBar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import {auth} from "../config/firebase";
-import { useAuthState } from 'react-firebase-hooks/auth'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import './navbar.css';
+
 export const Navbar = () => {
    const [user] = useAuthState(auth);
   return (
-  <div> 
-  <div className="Links"> 
-    <Link to="/">Home</Link>
-    <Link to="/login">Login</Link>
-  </div>
-
+  <div className="navBar"> 
+     <div className="link"> 
+      <Link className="home" to="/">Home</Link>
+      <Link className="login" to="/login">Login</Link>
+     </div>
+   
+    
     <div className="user">
       <p>{auth.currentUser?.displayName}</p>
       <img src={auth.currentUser?.photoURL || ""} width="100" height="100" />
