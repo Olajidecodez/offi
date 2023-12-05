f0d013f4e71cb5075683f5fac1cf0244cc1e0846
Olajide Balogun <olajidebalogun@icloud.com>
2023-12-05 21:55:50 +0100

    added log out button to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import {auth} from "../config/firebase";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from "firebase/auth";
 import './navbar.css';
 
 export const Navbar = () => {
    const [user] = useAuthState(auth);
+
+   const signUserOut = async () =>  {
+     await signOut(auth); 
+   };
+
   return (
   <div className="navBar"> 
      <div className="link"> 
@@ -13,9 +19,14 @@ export const Navbar = () => {
      </div>
    
     
-    <div className="user">
-      <p>{auth.currentUser?.displayName}</p>
-      <img src={auth.currentUser?.photoURL || ""} width="100" height="100" />
+    <div className="user"> 
+    { user && (
+      <>
+      <p>{user?.displayName}</p>
+      <img src={user?.photoURL || ""} width="100" height="100" />
+      <button onClick={signUserOut}> Log Out</button>
+      </>
+    )}
     </div>
   </div>
   )
