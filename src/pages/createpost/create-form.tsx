539277a73dd13bafe31aed5a6a50b35086bcd5c5
Olajide Balogun <olajidebalogun@icloud.com>
2023-12-06 12:54:31 +0100

    form schema

--- src/pages/createpost/create-form.tsx
+++ src/pages/createpost/create-form.tsx
@@ -2,9 +2,16 @@ import { useForm} from 'react-hook-form';
 import * as yup from "yup";
 
 export const CreateForm = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add a title."),
+    description: yup.string().required("You must add a description."),
+  });
+
    return (
      <form>
-      <input />
+      <input placeholder="Title..." />
+      <textarea placeholder="Description..." />
+      <input type="submit" />
      </form>
    );
 };
